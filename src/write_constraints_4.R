ec443b94b2488e87158dfa5cad154e1849e26b4a
enio23 <enio.gjerga@gmail.com>
2018-01-12 16:02:32 +0100

    Update write_constraints_4.R
    Here I fixed the bug for the cases when we had double interactions in the pkn with different signs.
    
    Former-commit-id: cfde679ee9b6ec0e6b712659d0eb3a062b319877

--- src/write_constraints_4.R
+++ src/write_constraints_4.R
@@ -9,16 +9,14 @@ write_constraints_4 <- function(variables=variables, conditionIDX=conditionIDX) 
                                variables$variables[match(paste0("Species ",
                                                                 unlist(strsplit(gsub(gsub(variables$exp[variables$idxEdgesUp[idx1]], pattern = "ReactionUp ", replacement = ""), pattern = paste0(" in experiment ", conditionIDX), replacement = ""), split = "="))[c(TRUE, FALSE)],
                                                                 " in experiment ", conditionIDX), variables$exp)], " - ", 
-                               variables$variables[match(gsub(variables$exp[variables$idxEdgesUp[idx1]], pattern = "ReactionUp ", replacement = "ReactionDown "), 
-                                                         variables$exp)],
+                               variables$uTable[match(variables$variables[variables$idxEdgesUp[idx1]], variables$uTable[, 1]), 2],
                                " <= 0")
   
   constraints4[idx2] <- paste0(variables$variables[variables$idxEdgesUp[idx2]], " + ",
                                variables$variables[match(paste0("Species ",
                                                                 unlist(strsplit(gsub(gsub(variables$exp[variables$idxEdgesUp[idx2]], pattern = "ReactionUp ", replacement = ""), pattern = paste0(" in experiment ", conditionIDX), replacement = ""), split = "="))[c(TRUE, FALSE)],
                                                                 " in experiment ", conditionIDX), variables$exp)], " - ",
-                               variables$variables[match(gsub(variables$exp[variables$idxEdgesUp[idx2]], pattern = "ReactionUp ", replacement = "ReactionDown "), 
-                                                         variables$exp)],
+                               variables$uTable[match(variables$variables[variables$idxEdgesUp[idx2]], variables$uTable[, 1]), 2],
                                " >= 0")
   
   return(constraints4)
