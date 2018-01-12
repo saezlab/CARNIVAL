0fe3511f97161c4e29cae595d26a26b80f180245
enio23 <enio.gjerga@gmail.com>
2018-01-12 16:03:48 +0100

    Update write_constraints_5.R
    Here I fixed the bugs related to the double interactions present in the pkn with different signs of interaction.
    
    Former-commit-id: 2504e059362fe30516946ee7beb3736c56c45636

--- src/write_constraints_5.R
+++ src/write_constraints_5.R
@@ -9,16 +9,14 @@ write_constraints_5 <- function(variables=variables, conditionIDX=conditionIDX) 
                                variables$variables[match(paste0("Species ",
                                                                 unlist(strsplit(gsub(gsub(variables$exp[variables$idxEdgesDown[idx1]], pattern = "ReactionDown ", replacement = ""), pattern = paste0(" in experiment ", conditionIDX), replacement = ""), split = "="))[c(TRUE, FALSE)],
                                                                 " in experiment ", conditionIDX), variables$exp)], " - ", 
-                               variables$variables[match(gsub(variables$exp[variables$idxEdgesDown[idx1]], pattern = "ReactionDown ", replacement = "ReactionUp "), 
-                                                         variables$exp)],
+                               variables$uTable[match(variables$variables[variables$idxEdgesDown[idx1]], variables$uTable[, 2]), 1],
                                " <= 0")
   
   constraints1[idx2] <- paste0(variables$variables[variables$idxEdgesDown[idx2]], " - ",
                                variables$variables[match(paste0("Species ",
                                                                 unlist(strsplit(gsub(gsub(variables$exp[variables$idxEdgesDown[idx2]], pattern = "ReactionDown ", replacement = ""), pattern = paste0(" in experiment ", conditionIDX), replacement = ""), split = "="))[c(TRUE, FALSE)],
                                                                 " in experiment ", conditionIDX), variables$exp)], " - ",
-                               variables$variables[match(gsub(variables$exp[variables$idxEdgesDown[idx2]], pattern = "ReactionDown ", replacement = "ReactionUp "), 
-                                                         variables$exp)],
+                               variables$uTable[match(variables$variables[variables$idxEdgesDown[idx2]], variables$uTable[, 2]), 1],
                                " >= 0")
   
   return(constraints1)
