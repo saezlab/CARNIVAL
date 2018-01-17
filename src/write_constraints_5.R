fb4e6dfe455cf20565ab26788a3372d77af7bd27
enio23 <enio.gjerga@gmail.com>
2018-01-17 14:34:34 +0100

    Update write_constraints_5.R
    Former-commit-id: 6fbad5153cc5f1221224d82609f7e53e6ff8ada5

--- src/write_constraints_5.R
+++ src/write_constraints_5.R
@@ -5,6 +5,22 @@ write_constraints_5 <- function(variables=variables, conditionIDX=conditionIDX) 
   idx1 <- which(variables$signs==1)
   idx2 <- which(variables$signs==-1)
   
+  # constraints1[idx1] <- paste0(variables$variables[variables$idxEdgesDown[idx1]], " + ",
+  #                              variables$variables[match(paste0("Species ",
+  #                                                               unlist(strsplit(gsub(gsub(variables$exp[variables$idxEdgesDown[idx1]], pattern = "ReactionDown ", replacement = ""), pattern = paste0(" in experiment ", conditionIDX), replacement = ""), split = "="))[c(TRUE, FALSE)],
+  #                                                               " in experiment ", conditionIDX), variables$exp)], " - ", 
+  #                              variables$variables[match(gsub(variables$exp[variables$idxEdgesDown[idx1]], pattern = "ReactionDown ", replacement = "ReactionUp "), 
+  #                                                        variables$exp)],
+  #                              " <= 0")
+  # 
+  # constraints1[idx2] <- paste0(variables$variables[variables$idxEdgesDown[idx2]], " - ",
+  #                              variables$variables[match(paste0("Species ",
+  #                                                               unlist(strsplit(gsub(gsub(variables$exp[variables$idxEdgesDown[idx2]], pattern = "ReactionDown ", replacement = ""), pattern = paste0(" in experiment ", conditionIDX), replacement = ""), split = "="))[c(TRUE, FALSE)],
+  #                                                               " in experiment ", conditionIDX), variables$exp)], " - ",
+  #                              variables$variables[match(gsub(variables$exp[variables$idxEdgesDown[idx2]], pattern = "ReactionDown ", replacement = "ReactionUp "), 
+  #                                                        variables$exp)],
+  #                              " >= 0")
+  
   constraints1[idx1] <- paste0(variables$variables[variables$idxEdgesDown[idx1]], " + ",
                                variables$variables[match(paste0("Species ",
                                                                 unlist(strsplit(gsub(gsub(variables$exp[variables$idxEdgesDown[idx1]], pattern = "ReactionDown ", replacement = ""), pattern = paste0(" in experiment ", conditionIDX), replacement = ""), split = "="))[c(TRUE, FALSE)],
