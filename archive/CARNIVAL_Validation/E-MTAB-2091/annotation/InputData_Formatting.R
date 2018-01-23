08c5b54998a32d884691897b5897b2e236873e79
Panuwat Trairatphisan <panuwat.trairatphisan@outlook.com>
2018-01-23 18:09:38 +0100

    Multiple updates on pipeline for validation
    - Fix issues with networks containing special characters that are not
    supported in LP file
    - Update R-scripts to generate measurement files (from DoRothEA) and
    input files (from direct targets & PROGENy representative proteins) for
    all compounds in the validation dataset.
    - Backup some intermediate results in the results
    - Update driver script (CRILPR -> CARNIVAL) to also export logged
    computational time for future references
    - New driver script designed specifically for iterative validation
    
    TODO:
    1) Fix SIF writing for SIGNOR network (optimisation runs ok) +/- find a
    way to deal with Babur network
    2) Write a new script to perform iterative validation with pp-Data
    
    Former-commit-id: 1f8a1318a80cf9bb39b3569ebca0cb13ad4e9c7c

--- archive/CARNIVAL_Validation/E-MTAB-2091/annotation/InputData_Formatting.R
+++ archive/CARNIVAL_Validation/E-MTAB-2091/annotation/InputData_Formatting.R
@@ -2,7 +2,7 @@
 
 rm(list=ls());cat("\014")
 
-setwd("~/Desktop/CARNIVAL_Validation/E-MTAB-2091/data/") # set working directory (relative)
+setwd("~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/archive/CARNIVAL_Validation/E-MTAB-2091/data/") # set working directory (relative)
 Dorothea <- read.table("E-MTAB-2091_DoRothEA.csv",header=T,sep=",",stringsAsFactors = F)
 Conditions <- Dorothea[,1]
 
@@ -31,20 +31,21 @@ plot(d_all,type="n")
 polygon(d_all,col="blue")
 
 # Defining cut-off
-sum(Progeny_Density_All < (-50) | Progeny_Density_All>50) # n=493 (from n=728)
+CutOffProgeny <- 50
+sum(Progeny_Density_All < (-CutOffProgeny) | Progeny_Density_All>CutOffProgeny) # n=493 (from n=728)
 
 # Combined data measurement file writing
-Progeny_CutOff_Abs_50 <- Progeny[,2:ncol(Progeny)]
-Progeny_CutOff_Abs_50[Progeny_CutOff_Abs_50<50 & Progeny_CutOff_Abs_50>(-50)] <- NaN
-Progeny_CutOff_Abs_50[Progeny_CutOff_Abs_50>=50] <- 1
-Progeny_CutOff_Abs_50[Progeny_CutOff_Abs_50<=(-50)] <- -1
-Progeny_CutOff_Abs_50[is.nan(as.matrix(Progeny_CutOff_Abs_50))] <- 0
-# ColNamesNew <- substring(colnames(Progeny_CutOff_Abs_50),5)
-# colnames(Progeny_CutOff_Abs_50) <- ColNamesNew
-write.table(x = Progeny_CutOff_Abs_50,file = "Progeny_CutOff_Abs_50_All",quote = F,sep = "\t",col.names = T,row.names = F)
+Progeny_CutOff <- Progeny[,2:ncol(Progeny)]
+Progeny_CutOff[Progeny_CutOff<50 & Progeny_CutOff>(-50)] <- NaN
+Progeny_CutOff[Progeny_CutOff>=50] <- 1
+Progeny_CutOff[Progeny_CutOff<=(-50)] <- -1
+Progeny_CutOff[is.nan(as.matrix(Progeny_CutOff))] <- 0
+# ColNamesNew <- substring(colnames(Progeny_CutOff),5)
+# colnames(Progeny_CutOff) <- ColNamesNew
+write.table(x = Progeny_CutOff,file = paste0("Progeny_All_CutOff_",toString(CutOffProgeny),".tsv"),quote = F,sep = "\t",col.names = T,row.names = F)
 
 
-setwd("~/Desktop/CARNIVAL_Validation/E-MTAB-2091/annotation/") # set working directory (relative)
+setwd("~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/archive/CARNIVAL_Validation/E-MTAB-2091/annotation/") # set working directory (relative)
 # Stimuli.csv
 Stimuli <- read.table("Stimuli.csv",header = T,sep = ",",stringsAsFactors = F)
 
@@ -60,14 +61,15 @@ for (counter in 1:nrow(Stimuli)) {
   All_Targets_Matrix[IdxCond,IdxTarget] <- Stimuli[counter,4]
 }
 
-write.table(x = All_Targets_Matrix,file = "DrugTarget_Input_All",quote = F,sep = "\t",col.names = T,row.names = F)
+write.table(x = All_Targets_Matrix,file = "DrugTarget_All",quote = F,sep = "\t",col.names = T,row.names = F)
 
 # Single input file writing
+# betaxolol
 All_Targets_betaxolol <- All_Targets_Matrix[1,]
 Idx_NonZero <- which(All_Targets_betaxolol!=0)
 All_Targets_betaxolol <- matrix(All_Targets_betaxolol[Idx_NonZero],1,length(Idx_NonZero))
 colnames(All_Targets_betaxolol) <- All_Targets[Idx_NonZero]
-write.table(x = All_Targets_betaxolol,file = "DrugTarget_Input_betaxolol.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
+write.table(x = All_Targets_betaxolol,file = "DrugTarget_betaxolol.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
 
 # === New variant : integration of Progeny score as additoinal inputs === #
 
@@ -82,23 +84,53 @@ for (counter in 1:length(unique(ProgenyProtein[,2]))) {
   ProgenyProtein_List[[counter]]$prot <- ProgenyProtein[Idx_pw_proteins,3]
 }
 
-# Now focus only on Betaxolol
-Progeny_CutOff_Abs_50_Betaxolol <- Progeny_CutOff_Abs_50[1,]
+# Load all compound names
+setwd("~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/archive/CARNIVAL_Validation/E-MTAB-2091/data/") # set working directory (relative)
+Compounds <- t(read.table("All_Compound_Names.tsv",sep = "\r"))
+setwd("~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/archive/CARNIVAL_Validation/E-MTAB-2091/annotation/") # set working directory (relative)
+
+# betaxolol
+Progeny_CutOff_Betaxolol <- Progeny_CutOff[1,]
 
 Progeny_Input_ToAdd <- NULL
-for (counter in 1:ncol(Progeny_CutOff_Abs_50_Betaxolol)) {
-  if (Progeny_CutOff_Abs_50_Betaxolol[counter]!=0) {
-    Idx_ProgenyList <- which(colnames(Progeny_CutOff_Abs_50_Betaxolol)[counter]==unique(ProgenyProtein[,2]))
+for (counter in 1:ncol(Progeny_CutOff_Betaxolol)) {
+  if (Progeny_CutOff_Betaxolol[counter]!=0) {
+    Idx_ProgenyList <- which(colnames(Progeny_CutOff_Betaxolol)[counter]==unique(ProgenyProtein[,2]))
     Matrix_ToAdd <- matrix(NA,2,length(ProgenyProtein_List[[Idx_ProgenyList]]$prot))
     Matrix_ToAdd[1,] <- ProgenyProtein_List[[Idx_ProgenyList]]$prot
-    Matrix_ToAdd[2,] <- Progeny_CutOff_Abs_50_Betaxolol[counter]
+    Matrix_ToAdd[2,] <- Progeny_CutOff_Betaxolol[counter]
     Progeny_Input_ToAdd <- cbind(Progeny_Input_ToAdd,Matrix_ToAdd)
   }
 }
 Progeny_Input <- matrix(Progeny_Input_ToAdd[2,],1,ncol(Progeny_Input_ToAdd))
 colnames(Progeny_Input) <- Progeny_Input_ToAdd[1,]
-write.table(x = cbind(All_Targets_betaxolol,Progeny_Input),file = "DrugTarget_Input_betaxolol_plusPROGENy.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
+write.table(x = cbind(All_Targets_betaxolol,Progeny_Input),file = paste0("DrugTarget_betaxolol_plusPROGENy_CutOff_",toString(CutOffProgeny) ,".tsv"),quote = F,sep = "\t",col.names = T,row.names = F)
 
-
+# all compounds
+for (counter_compound in 1:length(Compounds)) {
+  
+  # current compounds
+  All_Targets_current <- All_Targets_Matrix[counter_compound,]
+  Idx_NonZero <- which(All_Targets_current!=0)
+  All_Targets_current <- matrix(All_Targets_current[Idx_NonZero],1,length(Idx_NonZero))
+  colnames(All_Targets_current) <- All_Targets[Idx_NonZero]
+  write.table(x = All_Targets_current,file = paste0("DrugTarget_",Compounds[counter_compound],".tsv"),quote = F,sep = "\t",col.names = T,row.names = F)
+  
+  Progeny_CutOff_current <- Progeny_CutOff[counter_compound,]
+  
+  Progeny_Input_ToAdd <- NULL
+  for (counter in 1:ncol(Progeny_CutOff_current)) {
+    if (Progeny_CutOff_current[counter]!=0) {
+      Idx_ProgenyList <- which(colnames(Progeny_CutOff_current)[counter]==unique(ProgenyProtein[,2]))
+      Matrix_ToAdd <- matrix(NA,2,length(ProgenyProtein_List[[Idx_ProgenyList]]$prot))
+      Matrix_ToAdd[1,] <- ProgenyProtein_List[[Idx_ProgenyList]]$prot
+      Matrix_ToAdd[2,] <- Progeny_CutOff_current[counter]
+      Progeny_Input_ToAdd <- cbind(Progeny_Input_ToAdd,Matrix_ToAdd)
+    }
+  }
+  Progeny_Input <- matrix(Progeny_Input_ToAdd[2,],1,ncol(Progeny_Input_ToAdd))
+  colnames(Progeny_Input) <- Progeny_Input_ToAdd[1,]
+  write.table(x = cbind(All_Targets_current,Progeny_Input),file = paste0("DrugTarget_",Compounds[counter_compound],"_plusPROGENy_CutOff_",toString(CutOffProgeny) ,".tsv"),quote = F,sep = "\t",col.names = T,row.names = F)
+}
 
 # --- End of script --- #
--- archive/CARNIVAL_Validation/E-MTAB-2091/network/SIF_extraction.R
+++ archive/CARNIVAL_Validation/E-MTAB-2091/network/SIF_extraction.R
@@ -1,6 +1,6 @@
 # SIF file extraction from networks
 
-setwd("~/Desktop/CARNIVAL_Validation/E-MTAB-2091/network") # set working directory (relative)
+setwd("~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/archive/CARNIVAL_Validation/E-MTAB-2091/network") # set working directory (relative)
 
 # OmniPathRefHGNC.csv
 OmniPath <- read.table("OmniPathRefHGNC.csv",header = T,sep=",")
@@ -14,10 +14,19 @@ OmniPathSIF[,3] <- gsub("-","~",OmniPathSIF[,3],fixed = T)
 write.table(x = OmniPathSIF,file = "OmniPathSIF_NoHyphen.tsv",sep="\t",quote = F,row.names = F,col.names = F)
 
 # SIGNORfiltered.csv
-SIGNOR <- read.delim("SIGNORfiltered.csv",header = T,sep=",")
-SIGNORSIF <- SIGNOR[,2:4]
-SIGNORSIF <- SIGNORSIF[order(SIGNORSIF[,1],SIGNORSIF[,3]),]
-write.table(x = SIGNORSIF,file = "SignorSIF.tsv",quote = F,sep = "\t",col.names = F,row.names = F)
+Signor <- read.delim("SIGNORfiltered.csv",header = T,sep=",")
+SignorSIF <- Signor[,2:4]
+SignorSIF <- SignorSIF[order(SignorSIF[,1],SignorSIF[,3]),]
+write.table(x = SignorSIF,file = "SignorSIF.tsv",quote = F,sep = "\t",col.names = F,row.names = F)
+
+which(grepl(pattern = "-",x = SignorSIF[,1],fixed = T)) # indices to replace
+SignorSIF[,1] <- gsub("-","~",SignorSIF[,1],fixed = T)
+SignorSIF[,3] <- gsub("-","~",SignorSIF[,3],fixed = T)
+SignorSIF[,1] <- gsub("/","_sl_",SignorSIF[,1],fixed = T)
+SignorSIF[,3] <- gsub("/","_sl_",SignorSIF[,3],fixed = T)
+SignorSIF[,1] <- gsub(":","_cl_",SignorSIF[,1],fixed = T)
+SignorSIF[,3] <- gsub(":","_cl_",SignorSIF[,3],fixed = T)
+write.table(x = SignorSIF,file = "SignorSIF_NoHyphenNoSlashNoColon.tsv",sep="\t",quote = F,row.names = F,col.names = F)
 
 # BaburPPI.csv (PPI)
 Babur <- read.delim("BaburPPI.csv",header = T,sep=",")
@@ -26,7 +35,12 @@ BaburSIF <- BaburSIF[order(BaburSIF[,1],BaburSIF[,3]),]
 BaburSIF[,2] <- 1
 BaburSIF <- rbind(BaburSIF,BaburSIF) # doubled interactions
 BaburSIF[((nrow(BaburSIF)/2)+1):nrow(BaburSIF),2] <- -1 # set doubled interactions to inhibition
-
 write.table(x = BaburSIF,file = "BaburSIF.tsv",quote = F,sep = "\t",col.names = F,row.names = F)
+
+which(grepl(pattern = "-",x = BaburSIF[,1],fixed = T)) # indices to replace
+BaburSIF[,1] <- gsub("-","~",BaburSIF[,1],fixed = T)
+BaburSIF[,3] <- gsub("-","~",BaburSIF[,3],fixed = T)
+write.table(x = BaburSIF,file = "BaburSIF_NoHyphen.tsv",sep="\t",quote = F,row.names = F,col.names = F)
+
 
 # --- End of script --- #
