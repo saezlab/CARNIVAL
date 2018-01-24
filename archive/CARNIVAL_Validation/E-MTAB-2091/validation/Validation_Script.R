b21fbd642615e6e4cbe0077fd8109e06b2d01bb1
Panuwat Trairatphisan <panuwat.trairatphisan@outlook.com>
2018-01-24 17:57:54 +0100

    Validation results from all compounds
    - BkUp all results from all compounds in the result folder
    - Add the checking of cplex-results’ existence before writing SIF file
    - Re-write validation script for the whole compounds + export useful
    figures.
    
    
    Former-commit-id: a45c6077f9517150e55b277187c97c643bc5f610 [formerly 48f7f0c816766c1375db88e7c7cd1a7fea303282]
    Former-commit-id: 5b27fd173fd16bba52d8a2a82de2784d74a1b31a

--- archive/CARNIVAL_Validation/E-MTAB-2091/validation/Validation_Script.R
+++ archive/CARNIVAL_Validation/E-MTAB-2091/validation/Validation_Script.R
@@ -6,31 +6,144 @@ setwd("~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/archive/CARNIVAL_Validation/E-MTAB-
 # E-MTAB-2091_PP5min.csv & E-MTAB-2091_PP25min.csv
 PP5min <- read.table("E-MTAB-2091_PP5min.csv",header=T,sep=",",stringsAsFactors = F)
 PP25min <- read.table("E-MTAB-2091_PP25min.csv",header=T,sep=",",stringsAsFactors = F)
-MeasuredPP <- colnames(PP5min)[4:ncol(PP5min)]
+MeasuredPP <- colnames(PP5min)[4:ncol(PP5min)] # Same list and order between PP5min and PP25min
 
-# nodesActivity_Betaxolol.txt
-# NodeAct_Betaxolol <- read.delim("nodesActivity_Betaxolol.txt",header=T,sep="\t",stringsAsFactors = F)
-# NodeAct_Betaxolol <- read.delim("nodesActivity_Betaxolol_PROGENy.txt",header=T,sep="\t",stringsAsFactors = F)
-# NodeAct_Betaxolol <- read.delim("nodesActivity_Betaxolol_PROGENy_Update_Net1.txt",header=T,sep="\t",stringsAsFactors = F)
-# NodeAct_Betaxolol <- read.delim("nodesActivity_Betaxolol_PROGENy_Update_Net4.txt",header=T,sep="\t",stringsAsFactors = F)
-NodeAct_Betaxolol <- read.delim("formaldehyde/nodesActivity_formaldehyde_Net1.txt",header=T,sep="\t",stringsAsFactors = F)
-Overlapped_Proteins <- intersect(MeasuredPP,NodeAct_Betaxolol[,1])
+# Manual annotation of gene names with HGNC symbol (GeneCards)
+#                    "AKT1" "CREB1" "EGFR" "ERK1" "FAK1" "GSK3B" "HSP27" "IKBA" "JNK2" "MEK1" "MKK6" "NFKB" "p38MAPK" "P53" "P70S6K" "RPS6" "SHP2" "WNK1" "RSK1"  
+MeasuredPP_HGNC <- c("AKT1","CREB1","EGFR","MAPK3","PTK2","GSK3B","HSPB1","NFKBIA","MAPK9","MAP2K1","MAP2K6","NFKB1","MAPK14","TP53","RPS6KB1","RSPH6A","PTPN11","WNK1","RPS6KA1")
 
-# Writing results for the overlapped proteins
-Result_Matrix <- matrix(NA,length(Overlapped_Proteins),4)
-colnames(Result_Matrix) <- c("Protein","CARNIVAL","PP5min","PP25min")
-Idx_Condition <- which("betaxolol"==PP5min[,1])
-for (counter in 1:length(Overlapped_Proteins)) {
-  Current_Overlapped_Protein <- Overlapped_Proteins[counter]
-  Current_CARNIVAL_output <- NodeAct_Betaxolol$Activity[NodeAct_Betaxolol$Nodes==Current_Overlapped_Protein]
-  Current_PP5min_meas <- PP5min[Idx_Condition,Current_Overlapped_Protein==colnames(PP5min)]
-  Current_PP25min_meas <- PP25min[Idx_Condition,Current_Overlapped_Protein==colnames(PP25min)]
-  Result_Matrix[counter,] <- c(Current_Overlapped_Protein,Current_CARNIVAL_output,Current_PP5min_meas,Current_PP25min_meas)
+CompoundsPP <- PP5min[,1]
+CompoundsPP_All <- NULL
+for (counter in 1:length(CompoundsPP)) {
+  CompoundsPP_All[[counter]] <- strsplit(CompoundsPP[counter],split = " ",fixed = T)
+}
+CompoundsPP_Names <- NULL
+for (counter in 1:length(CompoundsPP_All)) {
+  if (length(CompoundsPP_All[[counter]][[1]])==1) {
+    CompoundsPP_Names[[counter]] <- CompoundsPP_All[[counter]][[1]][1]
+  } else {
+    if (grepl(pattern = "(",x = CompoundsPP_All[[counter]][[1]][2],fixed = T)) {
+      CompoundsPP_Names[[counter]] <- CompoundsPP_All[[counter]][[1]][1]
+    } else {
+      CompoundsPP_Names[[counter]] <- paste0(CompoundsPP_All[[counter]][[1]][1],"_",CompoundsPP_All[[counter]][[1]][2])
+    }
+  }
+}
+CompoundsPP_Names <- unlist(CompoundsPP_Names)
+
+# === All combined result === #
+Compounds <- t(read.table(file = "~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/archive/CARNIVAL_Validation/E-MTAB-2091/data/All_Compound_Names.tsv",sep = "\r"))
+
+ValResMat <- as.data.frame(matrix(NA,length(Compounds),length(MeasuredPP)*3))
+ValResMatCol <- NULL
+for (counter in 1:length(MeasuredPP)) {
+  ValResMatCol <- c(ValResMatCol,paste0(MeasuredPP[counter],"_Mod"),paste0(MeasuredPP[counter],"_5m"),paste0(MeasuredPP[counter],"_25m"))
+}
+colnames(ValResMat) <- ValResMatCol
+rownames(ValResMat) <- Compounds
+ValResMatSign <- ValResMat
+
+CutOff <- 0.1
+ValResMatSignCutOff <- ValResMat
+
+for (counter_compound in 1:length(Compounds)) {
+
+  print(paste0("Now mapping: ",Compounds[counter_compound]))
+  
+  if (file.exists(paste0("~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/results/validation_",Compounds[counter_compound],"_generic/nodesActivity_1.txt"))) {
+  
+    NodeAct_current <- read.delim(paste0("~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/results/validation_",Compounds[counter_compound],"_generic/nodesActivity_1.txt"),header=T,sep="\t",stringsAsFactors = F)
+    # NodeAct_current <- read.delim(paste0("~/Desktop/RWTH_Aachen/GitHub/CARNIVAL/results/validation_",Compounds[counter_compound],"_omnipath/nodesActivity_1.txt"),header=T,sep="\t",stringsAsFactors = F)
+    
+    Overlapped_Proteins <- intersect(MeasuredPP_HGNC,NodeAct_current[,1])
+  
+    if (length(Overlapped_Proteins)>0) {  
+      # Writing results for the overlapped proteins
+      Result_Matrix <- matrix(NA,length(Overlapped_Proteins),4)
+      colnames(Result_Matrix) <- c("Protein","CARNIVAL","PP5min","PP25min")
+      Idx_Condition <- which(Compounds[counter_compound]==CompoundsPP_Names)
+      for (counter in 1:length(Overlapped_Proteins)) {
+        Current_Overlapped_Protein <- Overlapped_Proteins[counter]
+        Current_CARNIVAL_output <- NodeAct_current$Activity[NodeAct_current$Nodes==Current_Overlapped_Protein]
+        Current_PP5min_meas <- PP5min[Idx_Condition,which(Current_Overlapped_Protein==MeasuredPP_HGNC)+3]
+        Current_PP25min_meas <- PP25min[Idx_Condition,which(Current_Overlapped_Protein==MeasuredPP_HGNC)+3]
+        Result_Matrix[counter,] <- c(Current_Overlapped_Protein,Current_CARNIVAL_output,Current_PP5min_meas,Current_PP25min_meas)
+        # map to summarised matrix
+        Idx_ValResMat <- which(Current_Overlapped_Protein==MeasuredPP_HGNC)
+        ValResMat[counter_compound,(((Idx_ValResMat-1)*3)+1):(((Idx_ValResMat-1)*3)+3)] <- c(Current_CARNIVAL_output,Current_PP5min_meas,Current_PP25min_meas)
+        ValResMatSign[counter_compound,(((Idx_ValResMat-1)*3)+1):(((Idx_ValResMat-1)*3)+3)] <- c(Current_CARNIVAL_output,sign(Current_PP5min_meas),sign(Current_PP25min_meas))
+        ValResMatSignCutOff[counter_compound,(((Idx_ValResMat-1)*3)+1):(((Idx_ValResMat-1)*3)+3)] <- c(Current_CARNIVAL_output,
+                                                                                                       sign(if (abs(Current_PP5min_meas)<=CutOff) {Current_PP5min_meas=0} else {Current_PP5min_meas}),                                                                                                       sign(if (abs(Current_PP25min_meas)<=CutOff) {Current_PP25min_meas=0} else {Current_PP25min_meas}))
+      }
+      write.table(x = Result_Matrix,file = paste0("Validation_Results_",Compounds[counter_compound],"_generic.tsv"),quote = F,sep = "\t",col.names = T,row.names = F)
+    }
+  }
 }
 
-# write.table(x = Result_Matrix,file = "Validation_Results_Betaxolol.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
-# write.table(x = Result_Matrix,file = "Validation_Results_Betaxolol_Net1.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
-# write.table(x = Result_Matrix,file = "Validation_Results_Betaxolol_Net4.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
-write.table(x = Result_Matrix,file = "formaldehyde/Validation_Results_formaldehyde_Net1.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
+# Analysis on the difference of predicted node activity and measurement (here use ValResMatSignCutOff)
+DiffMat <- matrix(NA,3,length(MeasuredPP_HGNC))
+colnames(DiffMat) <- MeasuredPP
+rownames(DiffMat) <- c("PP5min","PP25min","NrData")
+
+for (counter_diff in 1:length(MeasuredPP)){
+  ModVal <- ValResMatSignCutOff[,(((counter_diff-1)*3)+1)]; ModVal <- ModVal[!is.na(ModVal)]
+  Meas5min <- ValResMatSignCutOff[,(((counter_diff-1)*3)+2)]; Meas5min <- Meas5min[!is.na(Meas5min)]
+  Meas25min <- ValResMatSignCutOff[,(((counter_diff-1)*3)+3)]; Meas25min <- Meas25min[!is.na(Meas25min)]
+  DiffMat[,counter_diff] <- c(sum(abs(ModVal-Meas5min))/length(Meas5min),sum(abs(ModVal-Meas25min))/length(Meas25min),length(Meas5min))
+}
+
+pdf("Hamming_Distance_generic.pdf")
+# pdf("Hamming_Distance_omnipath.pdf")
+plot(1:length(MeasuredPP),DiffMat[1,],type = 'p',col='red',cex=DiffMat[3,]/10,
+     xlab = "Measured-PP",ylab="Distance",
+     ylim = c(0,max(c(DiffMat[1,][(!is.na(DiffMat[1,]))],DiffMat[2,][(!is.na(DiffMat[2,]))])+0.5)),
+     main = "Average Hamming distance - Generic network",axes = F)
+     # main = "Average Hamming distance - Omnipath network",axes = F)
+axis(1, at=seq(1,length(MeasuredPP),by=1),labels=MeasuredPP, las = 2)
+axis(2, at=seq(0,max(c(DiffMat[1,][(!is.na(DiffMat[1,]))],DiffMat[2,][(!is.na(DiffMat[2,]))])+0.5),by=0.2),labels=seq(0,max(c(DiffMat[1,][(!is.na(DiffMat[1,]))],DiffMat[2,][(!is.na(DiffMat[2,]))])+0.5),by=0.2), las = 2)
+points(1:length(MeasuredPP),DiffMat[2,],type = 'p', col='blue',cex=DiffMat[3,]/10)
+lines(1:length(MeasuredPP),rep(1,length(MeasuredPP)),lty=2,col="grey")
+legend("topright", legend=c("PP-5min", "PP-25min"),col=c("red", "blue"), lty=1, cex=0.8,inset = 0.02)
+dev.off()
+
+write.table(x = ValResMat,file = paste0("Summary_Validation_Results_generic.tsv"),quote = F,sep = "\t",col.names = T,row.names = T)
+write.table(x = ValResMatSign,file = paste0("Summary_Validation_Sign_Results_generic.tsv"),quote = F,sep = "\t",col.names = T,row.names = T)
+write.table(x = ValResMatSignCutOff,file = paste0("Summary_Validation_SignCutOff_Results_generic.tsv"),quote = F,sep = "\t",col.names = T,row.names = T)
+write.table(x = DiffMat,file = paste0("Summary_Validation_HammingDistance_generic.tsv"),quote = F,sep = "\t",col.names = T,row.names = T)
+
+# write.table(x = ValResMat,file = paste0("Summary_Validation_Results_omnipath.tsv"),quote = F,sep = "\t",col.names = T,row.names = T)
+# write.table(x = ValResMatSign,file = paste0("Summary_Validation_Sign_Results_omnipath.tsv"),quote = F,sep = "\t",col.names = T,row.names = T)
+# write.table(x = ValResMatSignCutOff,file = paste0("Summary_Validation_SignCutOff_Results_omnipath.tsv"),quote = F,sep = "\t",col.names = T,row.names = T)
+# write.table(x = DiffMat,file = paste0("Summary_Validation_HammingDistance_omnipath.tsv"),quote = F,sep = "\t",col.names = T,row.names = T)
+
+
+
+# # === Individual result === #
+# 
+# # NodeAct_Betaxolol <- read.delim("nodesActivity_Betaxolol.txt",header=T,sep="\t",stringsAsFactors = F)
+# # NodeAct_Betaxolol <- read.delim("nodesActivity_Betaxolol_PROGENy.txt",header=T,sep="\t",stringsAsFactors = F)
+# # NodeAct_Betaxolol <- read.delim("nodesActivity_Betaxolol_PROGENy_Update_Net1.txt",header=T,sep="\t",stringsAsFactors = F)
+# # NodeAct_Betaxolol <- read.delim("nodesActivity_Betaxolol_PROGENy_Update_Net4.txt",header=T,sep="\t",stringsAsFactors = F)
+# NodeAct_Betaxolol <- read.delim("formaldehyde/nodesActivity_formaldehyde_Net1.txt",header=T,sep="\t",stringsAsFactors = F)
+# Overlapped_Proteins <- intersect(MeasuredPP_HGNC,NodeAct_Betaxolol[,1])
+# 
+# # Writing results for the overlapped proteins
+# Result_Matrix <- matrix(NA,length(Overlapped_Proteins),4)
+# colnames(Result_Matrix) <- c("Protein","CARNIVAL","PP5min","PP25min")
+# Idx_Condition <- which("betaxolol"==PP5min[,1])
+# for (counter in 1:length(Overlapped_Proteins)) {
+#   Current_Overlapped_Protein <- Overlapped_Proteins[counter]
+#   Current_CARNIVAL_output <- NodeAct_Betaxolol$Activity[NodeAct_Betaxolol$Nodes==Current_Overlapped_Protein]
+#   Current_PP5min_meas <- PP5min[Idx_Condition,which(Current_Overlapped_Protein==MeasuredPP_HGNC)+3]
+#   Current_PP25min_meas <- PP25min[Idx_Condition,which(Current_Overlapped_Protein==MeasuredPP_HGNC)+3]
+#   Result_Matrix[counter,] <- c(Current_Overlapped_Protein,Current_CARNIVAL_output,Current_PP5min_meas,Current_PP25min_meas)
+# }
+# 
+# # write.table(x = Result_Matrix,file = "Validation_Results_Betaxolol.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
+# # write.table(x = Result_Matrix,file = "Validation_Results_Betaxolol_Net1.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
+# # write.table(x = Result_Matrix,file = "Validation_Results_Betaxolol_Net4.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
+# write.table(x = Result_Matrix,file = "formaldehyde/Validation_Results_formaldehyde_Net1.tsv",quote = F,sep = "\t",col.names = T,row.names = F)
+# 
+
 
 # --- End of script --- #
