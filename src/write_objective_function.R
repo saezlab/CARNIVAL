daa3d052ace2d9fd21efc5d40a76c46c7274f197
Panuwat Trairatphisan <panuwat.trairatphisan@outlook.com>
2018-02-15 18:38:51 +0100

    [Unfinished] Weight implementation by Enio
    - Applied custom weight implementation on CARNIVAL’s objective function
    to re-direct solution based on pathway score (e.g. PROGENy)
    - WriteLPFile and write_objective_function_all functions are just to
    update “weights”, “scores” and set alpha/beta.
    - Lots of changes (not yet final) on write_objective_function -> keep
    backup copy (BkUp) just in case
    
    
    Former-commit-id: 5deaf1dc031483434cb0c5b7fca769856edeef84

--- src/write_objective_function.R
+++ src/write_objective_function.R
@@ -1,33 +1,139 @@
-write_objective_function <- function(dataMatrix = dataMatrix, variables = variables, alpha=alpha, beta=beta){
+write_objective_function <- function(dataMatrix = dataMatrix, variables = variables, scores = scores, weights = weights, alpha=alpha, beta=beta){
   
-  measured <- gsub(colnames(dataMatrix$dataMatrix)[dataMatrix$dsID], pattern = "DS:", replacement = "")
-  measured <- c(measured, gsub(colnames(dataMatrix$dataMatrix)[dataMatrix$dnID], pattern = "DN:", replacement = ""))
-  
-  idxMeasured <- c()
-  for(i in 1:length(measured)){
+  if(is.null(scores)){
     
-    idxMeasured <- c(idxMeasured, which(variables$expNodesReduced==paste0("Species ", measured[i])))
+    measured <- gsub(colnames(dataMatrix$dataMatrix)[dataMatrix$dsID], pattern = "DS:", replacement = "")
+    measured <- c(measured, gsub(colnames(dataMatrix$dataMatrix)[dataMatrix$dnID], pattern = "DN:", replacement = ""))
+    
+    idxMeasured <- c()
+    for(i in 1:length(measured)){
+      
+      idxMeasured <- c(idxMeasured, which(variables$expNodesReduced==paste0("Species ", measured[i])))
+      
+    }
+    
+    measuredVar <- variables$variables[idxMeasured]
+    
+    objectiveFunctionVec <- paste0(" + ", alpha, " absDiff", gsub(measuredVar, pattern = "xb", replacement = ""))
+    objectiveFunction <- paste(objectiveFunctionVec, collapse = "")
+    
+    objectiveFunction <- substring(text = objectiveFunction[1], first = 4, last = nchar(objectiveFunction))
+    
+    objectiveFunction <- paste0("", objectiveFunction)
+    
+    objectiveFunctionUpVec <- paste0(" + ", beta, " ", variables$variables[variables$idxNodesUp])
+    objectiveFunctionUp <- paste(objectiveFunctionUpVec, collapse = "")
+    
+    objectiveFunctionDownVec <- paste0(" + ", beta, " ", variables$variables[variables$idxNodesDown])
+    objectiveFunctionDown <- paste(objectiveFunctionDownVec, collapse = "")
+    
+    objectiveFunction <- paste0(objectiveFunction, objectiveFunctionUp)
+    objectiveFunction <- paste0(objectiveFunction, objectiveFunctionDown)
+    
+    return(objectiveFunction)
+    
+  } else {
+    
+    measured <- gsub(colnames(dataMatrix$dataMatrix)[dataMatrix$dsID], pattern = "DS:", replacement = "")
+    measured <- c(measured, gsub(colnames(dataMatrix$dataMatrix)[dataMatrix$dnID], pattern = "DN:", replacement = ""))
+    
+    idxMeasured <- c()
+    for(i in 1:length(measured)){
+      
+      idxMeasured <- c(idxMeasured, which(variables$expNodesReduced==paste0("Species ", measured[i])))
+      
+    }
+    
+    measuredVar <- variables$variables[idxMeasured]
+    
+    objectiveFunctionVec <- paste0(" + ", alpha, " absDiff", gsub(measuredVar, pattern = "xb", replacement = ""))
+    objectiveFunction <- paste(objectiveFunctionVec, collapse = "")
+    
+    objectiveFunction <- substring(text = objectiveFunction[1], first = 4, last = nchar(objectiveFunction))
+    
+    objectiveFunction <- paste0("", objectiveFunction)
+    
+    # weights <- scores
+    # absoluteWeights <- abs(weights)
+    # for(i in 1:length(weights)){
+    #   
+    #   # weights[i] <- sign(scores[i])*(1-(absoluteWeights[i] - min(absoluteWeights, na.rm = TRUE))/(max(absoluteWeights, na.rm = TRUE) - min(absoluteWeights, na.rm = TRUE)))
+    #   weights[i] <- 1 - 2*(absoluteWeights[i] - min(absoluteWeights, na.rm = TRUE))/(max(absoluteWeights, na.rm = TRUE) - min(absoluteWeights, na.rm = TRUE))
+    #   
+    # }
+    # absoluteWeights <- abs(weights)
+    
+    weightedSpecies <- paste0("Species ", names(weights))
+    
+    ##
+    idx1 <- which(variables$expNodesReduced%in%weightedSpecies[which(scores > 0)])
+    
+    if(length(idx1) > 0){
+      
+      objectiveFunctionUp1 <- ""
+
+      for(i in 1:length(idx1)){
+        
+        ww <- weights[which(weightedSpecies==variables$expNodesReduced[idx1[i]])]
+        
+        if(sign(ww)==1){
+          
+          objectiveFunctionUpVec <- paste0(" + ", beta*ww, " ", variables$variables[variables$idxNodesUp[idx1[i]]])
+          objectiveFunctionUp1 <- paste(objectiveFunctionUp1, objectiveFunctionUpVec, sep = "")
+          
+        } else {
+          
+          objectiveFunctionUpVec <- paste0(" - ", abs(beta*ww), " ", variables$variables[variables$idxNodesUp[idx1[i]]])
+          objectiveFunctionUp1 <- paste(objectiveFunctionUp1, objectiveFunctionUpVec, sep = "")
+          
+        }
+        
+      }
+      
+    }
+    
+    ##
+    idx2 <- which(variables$expNodesReduced%in%weightedSpecies[which(scores < 0)])
+    
+    objectiveFunctionDown1 <- ""
+    
+    if(length(idx2) > 0){
+      
+      for(i in 1:length(idx2)){
+        
+        ww <- weights[which(weightedSpecies==variables$expNodesReduced[idx2[i]])]
+        
+        if(sign(ww)==1){
+          
+          objectiveFunctionDownVec <- paste0(" + ", beta*ww, " ", variables$variables[variables$idxNodesDown[idx2[i]]])
+          objectiveFunctionDown1 <- paste(objectiveFunctionDown1, objectiveFunctionDownVec, sep = "")
+          
+        } else {
+          
+          objectiveFunctionDownVec <- paste0(" - ", abs(beta*ww), " ", variables$variables[variables$idxNodesDown[idx2[i]]])
+          objectiveFunctionDown1 <- paste(objectiveFunctionDown1, objectiveFunctionDownVec, sep = "")
+          
+        }
+        
+      }
+      
+    }
+    
+    ##
+    
+    objectiveFunctionUpVec <- paste0(" + ", beta, " ", variables$variables[variables$idxNodesUp[setdiff(1:length(variables$idxNodesUp), idx1)]])
+    objectiveFunctionUp2 <- paste(objectiveFunctionUpVec, collapse = "")
+
+    objectiveFunctionDownVec <- paste0(" + ", beta, " ", variables$variables[variables$idxNodesDown[setdiff(1:length(variables$idxNodesDown), idx2)]])
+    objectiveFunctionDown2 <- paste(objectiveFunctionDownVec, collapse = "")
+    
+    objectiveFunction <- paste0(objectiveFunction, paste0(objectiveFunctionUp1, objectiveFunctionUp2))
+    objectiveFunction <- paste0(objectiveFunction, paste0(objectiveFunctionDown1, objectiveFunctionDown2))
+    
+    write(objectiveFunction, file = "objFun.txt")
+    
+    return(objectiveFunction)
     
   }
-  
-  measuredVar <- variables$variables[idxMeasured]
-  
-  objectiveFunctionVec <- paste0(" + ", alpha, " absDiff", gsub(measuredVar, pattern = "xb", replacement = ""))
-  objectiveFunction <- paste(objectiveFunctionVec, collapse = "")
-  
-  objectiveFunction <- substring(text = objectiveFunction[1], first = 4, last = nchar(objectiveFunction))
-  
-  objectiveFunction <- paste0("", objectiveFunction)
-  
-  objectiveFunctionUpVec <- paste0(" + ", beta, " ", variables$variables[variables$idxNodesUp])
-  objectiveFunctionUp <- paste(objectiveFunctionUpVec, collapse = "")
-  
-  objectiveFunctionDownVec <- paste0(" + ", beta, " ", variables$variables[variables$idxNodesDown])
-  objectiveFunctionDown <- paste(objectiveFunctionDownVec, collapse = "")
-  
-  objectiveFunction <- paste0(objectiveFunction, objectiveFunctionUp)
-  objectiveFunction <- paste0(objectiveFunction, objectiveFunctionDown)
-  
-  return(objectiveFunction)
   
 }
