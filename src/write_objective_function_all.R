690bc8a6ad99ff9365aade801df250034e3c81cd
enio23 <enio.gjerga@gmail.com>
2018-06-21 12:07:10 +0200

    Update
    Update of the writing of the objective function by considering the measurements weights
    
    Former-commit-id: a46c22388d85681568dae5a02f7cdbcb3bdb4142

--- src/write_objective_function_all.R
+++ src/write_objective_function_all.R
@@ -1,6 +1,16 @@
-write_objective_function_all <- function(dataMatrix = dataMatrix, variables = variables,alphaWeight=alphaWeight, betaWeight=betaWeight,scores=scores,nodeWeights=nodeWeights ) {
+write_objective_function_all <- function(dataMatrix = dataMatrix, variables = variables,alphaWeight=alphaWeight, betaWeight=betaWeight,scores=scores,nodeWeights=nodeWeights, measuremetntsWeights=measuremetntsWeights ) {
   
   OF <- "Obj:\t"
+  
+  if(!is.null(measuremetntsWeights)){
+    
+    if(nrow(measuremetntsWeights)!=nrow(dataMatrix$dataMatrix)){
+      
+      stop("Number of rows of the Measurements-Weights table should be the same as the number of conditions considered..")
+      
+    }
+    
+  }
   
   for (i in 1:nrow(dataMatrix$dataMatrix)) {
     
@@ -8,12 +18,22 @@ write_objective_function_all <- function(dataMatrix = dataMatrix, variables = va
     dM$dataMatrix <- as.matrix(t(dataMatrix$dataMatrix[i, ]))
     dM$dataMatrixSign <- as.matrix(t(dataMatrix$dataMatrixSign[i, ]))
     
+    if(!is.null(measuremetntsWeights)){
+      
+      mm <- as.matrix(measuremetntsWeights[i, ])
+      
+    } else {
+      
+      mm <- NULL
+      
+    }
+    
     var <- variables[[i]]
     
     if (i==1) {
-      OF <- paste0(OF, write_objective_function(dataMatrix = dM, variables = var,alphaWeight = alphaWeight, betaWeight = betaWeight,scores=scores,nodeWeights=nodeWeights))
+      OF <- paste0(OF, write_objective_function(dataMatrix = dM, variables = var,alphaWeight = alphaWeight, betaWeight = betaWeight,scores=scores,nodeWeights=nodeWeights,measuremetntsWeights = mm))
     } else {
-      OF <- paste0(OF, " + ",  write_objective_function(dataMatrix = dM, variables = var, alphaWeight = alphaWeight, betaWeight = betaWeight,scores=scores,nodeWeights=nodeWeights))
+      OF <- paste0(OF, " + ",  write_objective_function(dataMatrix = dM, variables = var, alphaWeight = alphaWeight, betaWeight = betaWeight,scores=scores,nodeWeights=nodeWeights,measuremetntsWeights = mm))
     }
     
   }
