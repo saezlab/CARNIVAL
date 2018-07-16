24bebbde211823a05694d62e184146f8994a0cdc
Anika Liu <anikaliu@yahoo.de>
2018-07-16 12:59:42 +0200

    Added parallelised version of classical CARNIVAL
    Former-commit-id: d273af7b46c8d8e07657f07e9718c61e16169284

---
+++ CARNIVAL_parallel.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+#BSUB -J CARNIVAL_job
+#BSUB -W 100:00
+#BSUB -M 100000
+#BSUB -B -N -u panuwat.trairatphisan@gmail.com
+
+### Change to the work directory
+cd /home/pt178902/InvCARNIVAL
+
+for i in {1..3}
+do
+for cond in {'a','b','c'}
+do
+bsub -P jrc_combine -J CARNIVAL_job -W 100:00 -M 100000 -o $output_dir/output_${i}.txt -e $output_dir/error_${i}.txt command Rscript InverseCR_Driver_Parallel.R $i $cond
+sleep 10s
+done
+done
