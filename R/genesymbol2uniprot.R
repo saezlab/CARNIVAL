e22dc1add4f63969aa4ca21dd7e918b0ae86a430
Anika Liu <anikaliu@yahoo.de>
2018-11-11 10:46:15 +0100

    Updated preprocess functions
    Added preprocessing vignette and updated functions for DoRothea
    preprocessing.
    
    
    Former-commit-id: ae78c6d2151fe63bd057edab1df1ce8b8d70edb4 [formerly f0de491bb37529598783fc09bc75f13fbd558e99]
    Former-commit-id: e594d752ca3e777792b1173df2c78004947493d9

---
+++ R/genesymbol2uniprot.R
@@ -0,0 +1,23 @@
+#'\code{generate_measfile}
+#'
+#' This function converts the gene symbol rownames of a dataframe to uniprot rownames. In case one gene symbol maps to two uniprot IDs, the row will be duplicated.
+#' 
+#' @param names A vector of genes.
+#' @param map A dataframe with a uniprot ID column and a genesymbol column.
+#' @param geneID Column index of the gene symbol column in map.
+#' @param uniprotID Column index of the uniprot column in map.
+#' 
+#' @import tidyverse
+#' @return A vector of genes or uniprot IDs.
+
+
+genesymbol2uniprot=function(df, map, geneID, uniprotID){
+  
+  df$genesymbols_for_mapping<-rownames(df)
+  colnames(map)[geneID]<-'genesymbols_for_mapping'
+  colnames(map)[uniprotID]<-'uniprotids_for_mapping'
+  df<-dplyr::left_join(df, map)
+  colnames(df)<-df$uniprotids_for_mapping
+  df<-df%>%dplyr::select(-uniprotids_for_mapping,genesymbols_for_mapping)
+}
+  
