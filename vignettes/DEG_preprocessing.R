a795df21ec005d7cdfbfc4bd1f6f6b683b653541
Anika Liu <anikaliu@yahoo.de>
2018-11-21 14:11:26 +0100

    PROGENy calc
    Added PROGENy calc
    
    
    Former-commit-id: b4d9607db10c18286cf1f7936a1740c6880855f1

--- vignettes/DEG_preprocessing.R
+++ vignettes/DEG_preprocessing.R
@@ -3,15 +3,28 @@
 # ---------------------- #
 
 # set working directory and clear working environment
-# setwd("/CARNIVAL")
+ setwd("~/Documents/GitHub/CARNIVAL/")
 rm(list=ls());cat("\014");if(length(dev.list())>0){dev.off()}
 
 library(devtools)
 load_all() # load CARNIVAL package
 
+#Generate TF input files
 df<-read.csv2("inst/SBV_EGF_tvalues.csv", row.names = 'GeneName')  
 load("~/DoRothEA/data/TFregulons/Robjects_VIPERformat/consensus/BEST_viperRegulon.rdata")
+map<-read.csv("inst/dorothea_TF_mapping.csv")
 
-TF_activities<-run_dorothea(df, regulon=viper_regulon, confidence_level=c('A','B','C'))
-#Need to convert Genesymbol to Uniprot 
-generate_measfile(measurements=TF_activities, topnumber=50, write2folder="./measurements")
+TF_genesymbol<-run_dorothea(df, regulon=viper_regulon, confidence_level=c('A','B','C'))
+TF_uniprot<-genesymbol2uniprot(TF_genesymbol, map, 1, 2)
+generate_measfile(measurements=TF_uniprot, topnumber=50, write2folder="./measurements")
+
+#Generate PROGENy input files
+weight_matrix<-read.csv("inst/model_NatComm+14_human.csv")
+df_genenames<-data.frame('gene'=rownames(df),df)
+
+pathway_scores<-runPROGENy(df_genenames,weight_matrix, z_scores = F)
+
+for (cond in colnames(pathway_scores)){
+  scores<-rbind(rownames(pathway_scores),pathway_scores[,cond])
+  write.table(scores, paste0("../data/measurements/scores_",cond,".txt"),col.names = F, row.names = F, quote = F)
+}
