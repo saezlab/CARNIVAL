73215ad0dddb091f02d01308f4cd7f6c91d3c8a2
enio23 <enio.gjerga@gmail.com>
2018-11-29 14:45:42 +0100

    Documentation
    Former-commit-id: 72787cd58204451e52072d1a5d0af828bdbcd1f4

--- R/write_constraints_3_all.R
+++ R/write_constraints_3_all.R
@@ -1,3 +1,8 @@
+#'\code{write_constraints_3_all}
+#'
+#'@param variables Contains the list of variables as used to formulate the ILP problem, explanations for each variable and a list of useful indeces.
+#'@return This code writes the list of constraints (3) of the ILP problem for all the conditions.
+
 write_constraints_3_all <- function(variables=variables) {
 
   # ======================================= #
