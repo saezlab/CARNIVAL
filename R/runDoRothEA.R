8b63949c1efb4bfb3832352d07bb7384a8f62894
Panuwat Trairatphisan <panuwat.trairatphisan@outlook.com>
2018-12-17 19:45:29 +0100

    Vignette for (Anika's) DEG Pre-processing pipeline
    - Build a new vignette for DEG pre-processing (detailed description on
    tutorial still to be added)
    
    - Export all functions in the DEG pre-processing pipeline via Roxygen
    to be able to be called from the vignette
    
    - Rename some of the functions e.g. run_dorothea -> runDoRothEA to be
    more consistent with the other functions in the package e.g. runPROGENy
    and runCARNIVAL.

--- R/runDoRothEA.R
+++ R/runDoRothEA.R
@@ -1,17 +1,22 @@
-#'\code{run_dorothea}
+#'\code{runDoRothEA}
 #'
 #' This function infers TF activities based on the DoRothEA regulon and the viper function.
 #' The regulon can be filtered by confidence level (A-E).
-#' 
+#'
 #' @param confidence_level A vector describing which confidence levels of the DoRothEA regulon to use
 #' @param regulon DoRothEA regulon list
 #' @param df A n*m dataframe describing the contrast t-statistics, where n is the number of genes and m the number of conditions.
 #' @param write2file Path to outputfile for all TF_activities if desired
+#'
 #' @import tidyverse
 #' @import viper
+#'
 #' @return A n*m dataframe describing the inferred TF activities, where n is the number of Tfs and m the number of conditions.
+#'
+#' @export
 
-run_dorothea<-function(df, regulon, confidence_level=c('A','B','C'), write2file = NULL){
+runDoRothEA<-function(df, regulon, confidence_level=c('A','B','C'), write2file = NULL){
+  library(tidyverse)
   names(regulon) <- sapply(strsplit(names(viper_regulon), split = ' - '), head, 1)
   filtered_regulon <- regulon %>%
     map_df(.f = function(i) {
@@ -28,10 +33,10 @@ run_dorothea<-function(df, regulon, confidence_level=c('A','B','C'), write2file 
       targets = setNames(dat$mor, dat$target)
       likelihood = dat$likelihood
       list(tfmode =targets, likelihood = likelihood)})
-  
+
   TF_activities = as.data.frame(viper::viper(eset = df, regulon = filtered_regulon, nes = T, method = 'none', minsize = 4, eset.filter = F))
   if(!is.null(write2file)){write.csv2(TF_activities, file = write2file)}
-  
+
     return(TF_activities)
 }
 
