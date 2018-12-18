3cc250f79c965970b59fbe24f9973de253456b2e
Anika Liu <anikaliu@yahoo.de>
2018-12-18 16:38:50 +0100

    Remove warning from joining
    Former-commit-id: e13d9905a189ce5883cbaaba4d9cee42be6e22d9 [formerly 226274d9e5f88bd751202052b66ae04bcc44f181] [formerly 75f1fc57e4cc75df7155dce3d3518cd777eb7cf4 [formerly 2beb6474f580e853b1b36b2a30a634b5a8354d20]]
    Former-commit-id: b8f375d670cb70cad2bad56287208fc69f004049
    Former-commit-id: d4c2b07da7158239e9ce474685cf750784a2569a

--- R/genesymbol2uniprot.R
+++ R/genesymbol2uniprot.R
@@ -19,7 +19,7 @@ GeneSymbol2Uniprot=function(df, map, geneID=1, uniprotID=2){
   df$genesymbols_for_mapping<-rownames(df)
   colnames(map)[geneID]<-'genesymbols_for_mapping'
   colnames(map)[uniprotID]<-'uniprotids_for_mapping'
-  df<-dplyr::left_join(df, map)
+  df<-dplyr::left_join(df, map, by ="genesymbols_for_mapping")
   rownames(df)<-df$uniprotids_for_mapping
   df<-df%>%dplyr::select(-uniprotids_for_mapping,-genesymbols_for_mapping)
 }
