5795413322e47e584c32269d15a1aee3b01f62fd
enio23 <enio.gjerga@gmail.com>
2020-04-16 17:37:27 +0200

    Update to bioc guidelines
    Update to bioc guidelines
    
    
    Former-commit-id: f31f913423b0a6585d25ee5b0ee65ed0ee2085fa [formerly 9522592e66367b7e6674b33a23fced648f5f5fe5] [formerly ec082a8d78fb6ed8fea8b4bbbdde75fd0497df62 [formerly a28b7e935ad3a2cfe1582eef7afad2857b6ea3a0]]
    Former-commit-id: 80c7c68834491ff759cbae49cdb9b091a35f5a43 [formerly 0e5f332de83a0462a8562779ccb8a8dcc9868cb8]
    Former-commit-id: 9b67fc46d777d28757adf2b330262e8052335850

--- R/all_constraints_wLoop.R
+++ R/all_constraints_wLoop.R
@@ -8,7 +8,8 @@ all_constraints_wLoop <- function(c0=c0, c1=c1, c2=c2, c3=c3, c4=c4, c5=c5,
   
   allConst <- c(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)
   
-  allConstraints <- paste0("c", 1:length(allConst), ":\t", allConst, "\t \t")
+  allConstraints <- paste0("c", seq_len(length(allConst)), 
+                           ":\t", allConst, "\t \t")
   
   if(length(grep(pattern = "NaN", x = allConstraints)) > 0){
     return(allConstraints[-grep(pattern = "NaN", x = allConstraints)])
