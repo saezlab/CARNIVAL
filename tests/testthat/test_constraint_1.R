ff239b0956c8773e0c1527054515cec4833b14de
Olga Ivanova <>
2021-04-21 14:55:33 +0200

    updated test

--- tests/testthat/test_constraint_1.R
+++ tests/testthat/test_constraint_1.R
@@ -3,18 +3,19 @@
 ## Matteo Spatuzzi 2021
 
 library(testthat)
-#### Create dummy data ####
 
-createDummyVariables <- function(nNodes=5, sign=1) {
+#### Create dummy data ####
+createDummyVariables <- function(nNode, sign=1) {
   variables <- c()
   variables$nodesDf <- data.frame(nodes=c("node1", "node2"), nodesVars=c("x1", "x2"))
   variables$edgesDf <- data.frame(Node1=c("node1"), Sign=c(sign), Node2=c("node2"), 
                                   edgesUpVars=c("e1"))
+  
   return(variables)
 }
 
 dummyData <- createDummyVariables()
-constraint1_expected <- paste0(dummyData$edgesDf$edgesUpVars, " - ", dummyData$edgesDf$Node1, " >= ", 0)
+constraint1_expected <- "e1 - x1 >= 0"
 constraint1 <- createConstraints_1_2_v2(variables=dummyData)
 
 # Testing constraint 1
