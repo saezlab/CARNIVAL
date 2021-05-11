3259cb7c1d5dc65e4acaa3b5b96a8ab1e621cb8f
Olga Ivanova <>
2021-05-12 01:59:59 +0200

    updating website

--- R/solvers_lpsolve__solve_lpsolve.R
+++ R/solvers_lpsolve__solve_lpsolve.R
@@ -3,7 +3,7 @@ solveWithLpSolve <- function(lpMatrix, carnivalOptions) {
   lpForm <- prepareLPMatrixSingle(lpMatrix, carnivalOptions)
   # lp solve assumes that all decision variables are positive. We should shift
   # the problem to the positive plane. 
-  lpForm <- shiftConstraintSpace(lpForm)
+  #lpForm <- shiftConstraintSpace(lpForm)
   
   lpSolutionResults <- lp(direction = "min", objective.in = lpForm$obj, 
                           const.mat = lpForm$con, const.dir = lpForm$dir, 
@@ -21,10 +21,8 @@ getSolutionMatrixLpSolve <- function(lpSolution) {
   solMatrix[, 2] <- lpSolution[["lpSolutionResults"]]
   
   rownames(solMatrix) <- solMatrix[, 1] 
-  solMatrix[, 2] <- shiftConstraintSpaceBack(solMatrix[, 2])
+  #solMatrix[, 2] <- shiftConstraintSpaceBack(solMatrix[, 2])
   solMatrix <- as.matrix(solMatrix[, 2])
   
-  
-  print(solMatrix)
   return(solMatrix)
 }
